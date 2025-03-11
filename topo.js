a8657e4868e7deb69d06f6917850b830aba115c9
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-03-11 18:28:36 -0300

    nova aula

--- topo.js
+++ topo.js
@@ -1,6 +1,8 @@
 const head=document.head
 const body=document.body
 
+
+
 const estilo="<link rel='stylesheet' type='text/css' href='estilo_topo.css' />"
 
 head.innerHTML+=estilo
@@ -10,13 +12,19 @@ nav.setAttribute("class","nav_bar")
 
 body.prepend(nav)
 
+
 const ul=document.createElement("ul")
+const div=document.createElement("div")
+
+
 const li=
-    "<li>home</li>"+
-    "<li>portifólio</li>"+
-    "<li>agenda</li>"+
-    "<li>localização</li>"
+    "<li><a href='index.html' >home</a></li>"+
+    "<li><a href='index.html' >agenda</a></li>"+
+    "<li><a href='index.html' >orçamento</a></li>"
 nav.appendChild(ul)
+nav.appendChild(div)
+div.innerHTML="<button id='btn_menu'><div class='cont_linha'><div class='linha t_1 mer'></div><div class='linha t_2'></div></div></button>"
 ul.innerHTML=li
 
+const btn_menu=document.getElementById("btn_menu")
 
